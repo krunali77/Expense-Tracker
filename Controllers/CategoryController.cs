db40c278d0e4185c5b73f7ddd77b6a71379a5966
krunali77 <19cskru040@ldce.ac.in>
2023-11-02 12:44:08 +0530

    initial

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Expense_Tracker.Models;
+
+namespace Expense_Tracker.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Category
+        public async Task<IActionResult> Index()
+        {
+              return _context.Categories != null ? 
+                          View(await _context.Categories.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Categories'  is null.");
+        }
+
+        // GET: Category/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories
+                .FirstOrDefaultAsync(m => m.CategoryId == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // GET: Category/CreateorEdit
+        public IActionResult CreateorEdit(int id=0)
+        {
+            if (id == 0)
+            {
+                return View(new Category()); 
+            }
+            // Editing an existing category
+            var category = _context.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound(); // Handle the case where the category with the given ID doesn't exist.
+            }
+            return View(category);
+        }
+
+        // POST: Category/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+       
+        public async Task<IActionResult> CreateorEdit([Bind("CategoryId,Title,Icon,Type")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (category.CategoryId == 0)
+                {
+                    // Creating a new category
+                    _context.Add(category);
+                }
+                else
+                {
+                    // Editing an existing category
+                    _context.Entry(category).State = EntityState.Modified;
+                }
+
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+
+        // GET: Category/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories
+                .FirstOrDefaultAsync(m => m.CategoryId == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // POST: Category/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Categories == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Categories'  is null.");
+            }
+            var category = await _context.Categories.FindAsync(id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoryExists(int id)
+        {
+          return (_context.Categories?.Any(e => e.CategoryId == id)).GetValueOrDefault();
+        }
+    }
+}
